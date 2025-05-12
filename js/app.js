e10d3c3d5258cd46d978df70b0b1fb9144ce542e
Danillo Matavelli <danillomatavelli26@gmail.com>
2025-05-12 11:00:59 -0300

    Função Adicionar uma nova tarefa

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const definirSaudacao = () => {
 // Chamada da função
 definirSaudacao();
 
-// Função para adicionar uma nova tarefa
+
 // Carrefar tarefas do localStorage ao iniciar
 const carregarTarefas = () => {
 
@@ -53,3 +53,72 @@ const carregarTarefas = () => {
         });
     });
 }
+
+// Função para adicionar uma nova tarefa
+const adicionarTarefa = () => {
+
+    const taskName = document.querySelector('#taskName').value;
+    const taskDescription = document.querySelector('#taskDescription').value;
+    const taskDate = document.querySelector('#taskDate').value;
+    const taskTime = document.querySelector('#taskTime').value;
+
+    if (taskName && taskDate && taskTime ) {
+        const taskList = document.querySelector('#taskList');
+
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task-list');
+
+        // Formatando a data no padrão dd/mm/aa
+        const dataFormatada = new Date(taskDate).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit',
+        });
+
+        // Código HTML que será injetado na DIV 'task-item'
+        const taskHTML = `
+        <h3>${taskName}</h3>
+        <p>${taskDescription}</p>
+        <p><strong>${taskDate}</strong> ${taskTime}</p>
+        <div class="task-actions">
+            <button class="complete-btn">Concluir</button>
+            <button class="edit-btn">Editar</button>
+            <button class="delete-btn">Excluir</button>
+        </div>
+        `;
+
+        // Injeção do código HTML com as tarefas do usuário
+        // Adicionar event listeners aos botões da tarefa
+        taskItem.querySelector('.complete-btn').addEventListener('click', function() {
+            marcarComoConcluida(this);
+        });
+
+        taskItem.querySelector('.edit-btn').addEventListener('click', function() {
+            editarTarefa(this);
+        });
+
+        taskItem.querySelector('.delete-btn').addEventListener('click', function() {
+            excluirTarefa(this);
+        });
+
+        // Criar objeto, converter em string e Salvar no localStorage
+        const tarefas = JSON.parse(localStorage.getItem('taerfas')) || [];
+        tarefas.push({
+            nome: taskName,
+            descricao: taskDescription,
+            data: taskDate,
+            hora: taskTime,
+            html: taskHTML
+        });
+
+        // Conversão do objeto para string
+        localStorage.setItem('tarefas', JSON.stringify(tarefas));
+
+        alert('Tarefa adicionada com sucesso!');
+        document.querySelector('#task-form').reset();
+    } else {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+    }
+
+}
+
